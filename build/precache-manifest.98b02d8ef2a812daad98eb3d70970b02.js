389f89dbd8c34d62a851674c43608db3612d57c4
Maria Rosolovska <feofanmaria19@gmail.com>
2022-01-30 20:39:20 +0200

    first commit

---
+++ build/precache-manifest.98b02d8ef2a812daad98eb3d70970b02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a631a5417a6536abfa785742c502d86c",
+    "url": "/Memory_game-React.git/index.html"
+  },
+  {
+    "revision": "87ebc40cf0dbc523c39d",
+    "url": "/Memory_game-React.git/static/css/main.a923a4a8.chunk.css"
+  },
+  {
+    "revision": "0b8623bd60e956529697",
+    "url": "/Memory_game-React.git/static/js/2.f829261c.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/Memory_game-React.git/static/js/2.f829261c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ebc40cf0dbc523c39d",
+    "url": "/Memory_game-React.git/static/js/main.ca4f3230.chunk.js"
+  },
+  {
+    "revision": "a26ccd6a9f8192ce909d",
+    "url": "/Memory_game-React.git/static/js/runtime-main.99c2b80c.js"
+  }
+]);
